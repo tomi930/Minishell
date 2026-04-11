8fe8c604bc8066bc10044959230fe68af346470e
tomi930 <rus.thomas133@gmail.com>
2026-04-11 02:27:51 +0200

    redirection file updated now redirection truncate the first file it passes by if needed

--- src/execution/redirection.c
+++ src/execution/redirection.c
@@ -1,38 +1,19 @@
 #include "minishell.h"
 
-static int	redirect_input(t_cmd *cmd)
+static int	apply_redir_out(t_redir *r)
 {
-	int	fd;
-
-	fd = open(cmd->infile, O_RDONLY);
-	if (fd == -1)
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(cmd->infile, 2);
-		ft_putendl_fd(": No such file or directory", 2);
-		return (-1);
-	}
-	dup2(fd, STDIN_FILENO);
-	close(fd);
-	if (cmd->is_heredoc)
-		unlink(cmd->infile);
-	return (0);
-}
-
-static int	redirect_output(t_cmd *cmd)
-{
-	int	fd;
 	int	flags;
+	int	fd;
 
-	if (cmd->append)
+	if (r->type == REDIR_APPEND)
 		flags = O_WRONLY | O_CREAT | O_APPEND;
 	else
 		flags = O_WRONLY | O_CREAT | O_TRUNC;
-	fd = open(cmd->outfile, flags, 0644);
+	fd = open(r->target, flags, 0644);
 	if (fd == -1)
 	{
 		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(cmd->outfile, 2);
+		ft_putstr_fd(r->target, 2);
 		ft_putendl_fd(": Permission denied", 2);
 		return (-1);
 	}
@@ -41,12 +22,39 @@ static int	redirect_output(t_cmd *cmd)
 	return (0);
 }
 
+static int	apply_redir(t_redir *r)
+{
+	int	fd;
+
+	if (r->type == REDIR_IN || r->type == REDIR_HEREDOC)
+	{
+		fd = open(r->target, O_RDONLY);
+		if (fd == -1)
+		{
+			ft_putstr_fd("minishell: ", 2);
+			ft_putstr_fd(r->target, 2);
+			ft_putendl_fd(": No such file or directory", 2);
+			return (-1);
+		}
+		dup2(fd, STDIN_FILENO);
+		close(fd);
+		if (r->type == REDIR_HEREDOC)
+			unlink(r->target);
+		return (0);
+	}
+	return (apply_redir_out(r));
+}
+
 int	setup_redirections(t_cmd *cmd)
 {
-	if (cmd->infile && redirect_input(cmd) == -1)
-		return (-1);
-	if (cmd->outfile && redirect_output(cmd) == -1)
-		return (-1);
+	t_redir	*r;
+
+	r = cmd->redirs;
+	while (r)
+	{
+		if (apply_redir(r) == -1)
+			return (-1);
+		r = r->next;
+	}
 	return (0);
 }
-//i'll rewrite it to cover redirection with t_redirs instead of infile/outfile
