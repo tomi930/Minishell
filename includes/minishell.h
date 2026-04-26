06e93cc739f9f0ecf2af97d3983d2f3b67195415
trus <trus@f1r4s7.paris.42.school>
2026-04-26 13:55:18 +0200

    small fix

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,16 +1,17 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/wait.h>
 # include <signal.h>
+# include <errno.h>
+# include <string.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/libft.h"
-# include <errno.h>
-# include <string.h>
 
 extern int g_exit_status;
 
